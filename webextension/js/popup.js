c1255f0e150ad3c406c8787536a38ef833b2c434
Paweł Chołaj <pawel.cholaj90@gmail.com>
2017-03-25 00:51:18 +0100

    We just noticed, that we have to change our planned login flow, so this code will probably be rewritten in next hour...

--- webextension/js/popup.js
+++ webextension/js/popup.js
@@ -2,6 +2,7 @@ var main = new Vue({
     el: '#main',
     data: {
         authenticated: false,
+        backend: false,
         credentials: {
             login: '',
             password: '',
@@ -10,56 +11,79 @@ var main = new Vue({
         form: {
             sent: false,
             error: ''
-        }
+        },
+        refresh: 30000,
+        counter: 0,
+        auctions: [
+
+        ],
     },
     methods: {
-        submit: function(){
+        submitForm: function(){
             this.form.sent = true;
 
-            axios.post('https://', {
-                login: this.credentials.login,
-                password: this.credentials.password
-            }).then(function (response) {
-                console.log(response);
+            if(this.backend){
+                axios.post('https://', {
+                    login: this.credentials.login,
+                    password: this.credentials.password
+                }).then(function (response) {
+                    console.log(response);
 
-                response = JSON.parse(response);
-                main.credentials.token = response.token;
-                main.authenticated = true;
-            }).catch(function (error) {
-                console.log(error);
+                    response = JSON.parse(response);
+                    main.credentials.token = response.token;
+                    main.authenticated = true;
+                }).catch(function (error) {
+                    console.log(error);
 
-                
-            });
+                });
+            }
+            else
+            {
+                this.authenticated = true;
+                $(document).ready(function(){
+                    $('ul.tabs').tabs();
+                });
+            }
         },
-        check: function(){
+        checkAuth: function(){
             chrome.storage.sync.get('token', function(items){
                 if(items.length > 0){
-                    main.credentials.token = items[0];
-                    main.authenticated = true;
+                    this.credentials.token = items[0];
+                    this.authenticated = true;
+
+                    $(document).ready(function(){
+                        $('ul.tabs').tabs();
+                    });
                 }
                 else
                 {
-                    
+
                 }
-            });
+            }.bind(this));
+        },
+        loadData: function(){
+            console.log('loadData');
+            if(this.authenticated)
+            {
+                if(this.backend){
+
+                }
+                else
+                {
+                    this.auctions.push({ 
+                        img: 'http://placehold.it/48x48', 
+                        title: 'title' + (this.counter++)
+                    });
+                }
+            }
         }
+    },
+    mounted: function(){
+        console.log('mounted');
+        this.checkAuth();
+        this.loadData();
+        setInterval(function(){
+            main.loadData();
+        }, this.refresh); 
     }
 })
-
-var auctionsList = new Vue({
-    el: '#auctionsList',
-    data: {
-        auctions: [
-            { 
-                img: 'http://placehold.it/48x48', 
-                title: 'title 1'
-            },
-            { 
-                img: 'http://placehold.it/48x48', 
-                title: 'title 2'
-            },
-        ]
-    }
-})
-
-
