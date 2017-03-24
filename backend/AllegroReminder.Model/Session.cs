a7d9ce8a0af2bf025caf3591b0b5d1a6326c7bf2
Mikolaj Trawinski <trawinski@ais.pl>
2017-03-24 22:12:19 +0100

    cleanup

---
+++ backend/AllegroReminder.Model/Session.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AllegroReminder.Model
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public Guid AccessToken { get; set; }
+    }
+}
